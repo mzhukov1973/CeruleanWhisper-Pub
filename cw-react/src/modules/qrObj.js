002a950f4c9637ad5f2de009406604614a7feca1
UmbraFerreus <mzhukov31415dev@gmail.com>
2017-09-26 08:06:28 +0300

    Updates

--- cw-react/src/modules/qrObj.js
+++
@@ -1,2 +0,0 @@
-function updateQR()  { qrCode.clear();      qrCode.makeCode('String to encode...');        }
-function updateQR2() { this.qrCode.clear(); this.qrCode.makeCode(qrData.generateQRText()); }
